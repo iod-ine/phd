9d20e29cdd599f7f20aea8eec67ed8c3730ba94e
Ivan Dubrovin <books.booksovich@yandex.com>
2024-04-04 22:26:08 +0300

    Reformat code in test_lmf.py

--- tests/test_lmf.py
+++ tests/test_lmf.py
@@ -8,7 +8,9 @@ from src.lmf import local_maxima_filter
 def point_cloud():
     """A tiny handmade point cloud with two local maxima of different heights.
 
-    Here is how it looks like (the numbers represent heights and the distances between neighboring points are 1):
+    Here is how it looks like (the numbers represent heights and the distances between
+    neighboring points are 1):
+
         2  0  0
         0  1  0
         0  0  3
@@ -25,27 +27,51 @@ def point_cloud():
 
 def test_lmf_finds_correct_maxima(point_cloud):
     """The function should correctly identify local maxima based in the parameters."""
-    detected_maxima = local_maxima_filter(point_cloud, window_size=1, height_threshold=0)
+    detected_maxima = local_maxima_filter(
+        point_cloud,
+        window_size=1,
+        height_threshold=0,
+    )
     assert np.all(detected_maxima == np.array([[0, 0, 2], [1, 1, 1], [2, 2, 3]]))
 
-    detected_maxima = local_maxima_filter(point_cloud, window_size=2, height_threshold=0)
+    detected_maxima = local_maxima_filter(
+        point_cloud,
+        window_size=2,
+        height_threshold=0,
+    )
     assert np.all(detected_maxima == np.array([[0, 0, 2], [2, 2, 3]]))
 
-    detected_maxima = local_maxima_filter(point_cloud, window_size=10, height_threshold=0)
+    detected_maxima = local_maxima_filter(
+        point_cloud,
+        window_size=10,
+        height_threshold=0,
+    )
     assert np.all(detected_maxima == np.array([[2, 2, 3]]))
 
-    detected_maxima = local_maxima_filter(point_cloud, window_size=1, height_threshold=1)
+    detected_maxima = local_maxima_filter(
+        point_cloud,
+        window_size=1,
+        height_threshold=1,
+    )
     assert np.all(detected_maxima == np.array([[0, 0, 2], [2, 2, 3]]))
 
-    detected_maxima = local_maxima_filter(point_cloud, window_size=1, height_threshold=2)
+    detected_maxima = local_maxima_filter(
+        point_cloud,
+        window_size=1,
+        height_threshold=2,
+    )
     assert np.all(detected_maxima == np.array([[2, 2, 3]]))
 
-    detected_maxima = local_maxima_filter(point_cloud, window_size=1, height_threshold=5)
+    detected_maxima = local_maxima_filter(
+        point_cloud,
+        window_size=1,
+        height_threshold=5,
+    )
     assert detected_maxima.size == 0
 
 
 def test_lmf_rejects_non_numpy_input():
-    """The function assumes its input is a Numpy array and should reject anything else."""
+    """The function assumes its input is a Numpy array and rejects anything else."""
 
     with pytest.raises(TypeError):
         local_maxima_filter([[0, 0, 0], [1, 1, 1]], window_size=1, height_threshold=0)
