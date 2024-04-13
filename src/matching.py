daa25b1a4b1c0b5d81049148d4ebf7912484d37e
Ivan Dubrovin <books.booksovich@yandex.com>
2024-04-13 21:41:01 +0300

    Add function for matching detected trees to GT

---
+++ src/matching.py
@@ -0,0 +1,95 @@
+import numpy as np
+import scipy
+
+
+def match_candidates(
+    ground_truth: np.ndarray,
+    candidates: np.ndarray,
+    *,
+    max_distance: float,
+    max_height_difference: float,
+) -> list[dict]:
+    """Match ground truth trees to candidates.
+
+    Args:
+        ground_truth (np.ndarray): Array of shape (N, 2) with positions of the ground
+            truth trees.
+        candidates (np.ndarray): Array of shape (M, 3) with positions and heights of
+            detected trees.
+        max_distance (float): Maximum distance between actual and potential trees to
+            consider them a matched pair.
+        max_height_difference (float): Maximum height difference between actual and
+            potential trees to consider them a matched pair.
+
+    """
+
+    distance_matrix = scipy.spatial.distance_matrix(
+        x=ground_truth[:, :2],
+        y=candidates[:, :2],
+    )
+    indices = np.nonzero(distance_matrix <= max_distance)  # (ground_truths, candidates)
+    distances = distance_matrix[indices]
+    sparse_distances = sorted((pair, d) for pair, d in zip(zip(*indices), distances))
+
+    ground_truth_matched_mask = np.zeros(ground_truth.shape[0], dtype=bool)
+    candidates_matched_mask = np.zeros(candidates.shape[0], dtype=bool)
+    out = []
+
+    for (i, j), distance in sparse_distances:
+        if (
+            np.isnan(ground_truth[i][2])
+            or abs(ground_truth[i][2] - candidates[j][2]) <= max_height_difference
+        ):
+            if ground_truth_matched_mask[i]:
+                candidates_matched_mask[j] = True
+                out.append(
+                    {
+                        "ground_truth": None,
+                        "candidate": tuple(candidates[j]),
+                        "class": "FP",
+                        "distance": None,
+                    }
+                )
+            elif candidates_matched_mask[j]:
+                ground_truth_matched_mask[i] = True
+                out.append(
+                    {
+                        "ground_truth": tuple(ground_truth[i]),
+                        "candidate": None,
+                        "class": "FN",
+                        "distance": None,
+                    }
+                )
+            else:
+                ground_truth_matched_mask[i] = True
+                candidates_matched_mask[j] = True
+                out.append(
+                    {
+                        "ground_truth": tuple(ground_truth[i]),
+                        "candidate": tuple(candidates[j]),
+                        "class": "TP",
+                        "distance": distance,
+                    }
+                )
+
+    out.extend(
+        {
+            "ground_truth": tuple(point),
+            "candidate": None,
+            "class": "FN",
+            "distance": None,
+        }
+        for point in ground_truth[~ground_truth_matched_mask]
+    )
+
+    out.extend(
+        {
+            "ground_truth": None,
+            "candidate": tuple(point),
+            "class": "FP",
+            "distance": None,
+        }
+        for point in candidates[~candidates_matched_mask]
+    )
+
+    return out
---
+++ tests/test_matching.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pytest
+
+from src.matching import match_candidates
+
+matches = {
+    "first": [
+        {
+            "ground_truth": (0, 0, 5),
+            "candidate": (0, 1, 3),
+            "class": "TP",
+            "distance": 1.0,
+        },
+        {
+            "ground_truth": None,
+            "candidate": (0, 2, 4),
+            "class": "FP",
+            "distance": None,
+        },
+    ],
+    "second": [
+        {
+            "ground_truth": (0, 0, 5),
+            "candidate": (0, 2, 4),
+            "class": "TP",
+            "distance": 2.0,
+        },
+        {
+            "ground_truth": None,
+            "candidate": (0, 1, 3),
+            "class": "FP",
+            "distance": None,
+        },
+    ],
+    "none": [
+        {
+            "ground_truth": (0, 0, 5),
+            "candidate": None,
+            "class": "FN",
+            "distance": None,
+        },
+        {
+            "ground_truth": None,
+            "candidate": (0, 1, 3),
+            "class": "FP",
+            "distance": None,
+        },
+        {
+            "ground_truth": None,
+            "candidate": (0, 2, 4),
+            "class": "FP",
+            "distance": None,
+        },
+    ],
+}
+
+
+@pytest.mark.parametrize(
+    "max_distance,max_height_difference,expected",
+    [
+        pytest.param(
+            5,
+            5,
+            matches["first"],
+            id="select_closest",
+        ),
+        pytest.param(
+            5,
+            1,
+            matches["second"],
+            id="height_threshold_one",
+        ),
+        pytest.param(
+            5,
+            0,
+            matches["none"],
+            id="height_threshold_all",
+        ),
+        pytest.param(
+            1,
+            5,
+            matches["first"],
+            id="distance_threshold_one",
+        ),
+        pytest.param(
+            0,
+            5,
+            matches["none"],
+            id="distance_threshold_all",
+        ),
+    ],
+)
+def test_match_candidates(
+    max_distance,
+    max_height_difference,
+    expected,
+):
+    actual = match_candidates(
+        ground_truth=np.array([[0, 0, 5]]),
+        candidates=np.array([[0, 1, 3], [0, 2, 4]]),
+        max_distance=max_distance,
+        max_height_difference=max_height_difference,
+    )
+    assert actual == expected
