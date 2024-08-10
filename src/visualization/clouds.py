ed9b09e695c4a9a35cf27edd869f40223e4dcd97
Ivan Dubrovin <books.booksovich@yandex.com>
2024-08-10 16:59:35 +0300

    Add docstring to scatter_point_cloud_3d

--- src/visualization/clouds.py
+++ src/visualization/clouds.py
@@ -9,11 +9,13 @@ def scatter_point_cloud_3d(
     figsize=None,
     elev=None,
     azim=None,
+    cmap=None,
 ) -> plt.Axes:
+    """Create a 3D scatter plot of a point cloud."""
     color = color if color is not None else xyz[:, 2]
     fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(projection="3d")
     ax.view_init(elev=elev, azim=azim)
-    ax.scatter(*np.rollaxis(xyz, 1), c=color, s=1)
+    ax.scatter(*np.rollaxis(xyz, 1), c=color, s=1, cmap=cmap)
     ax.set_aspect("equal")
     return ax
