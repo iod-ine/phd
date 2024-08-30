d826f6c47666c977b7e343aea31e2916bc6d58dd
Ivan Dubrovin <books.booksovich@yandex.com>
2024-08-30 15:20:10 +0300

    Add PointNet++ segmentor to pointnet submodule

--- src/models/pointnet/__init__.py
+++ src/models/pointnet/__init__.py
@@ -1,3 +1,4 @@
 """Implementations of the PointNet++ variations."""
 
 from src.models.pointnet.classifier import PointNet2Classifier
+from src.models.pointnet.semantic_segmentation import PointNet2SemanticSegmentor
