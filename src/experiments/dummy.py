3368156fb38b1d441d2f8e50242f3f7d964a8d47
Ivan Dubrovin <books.booksovich@yandex.com>
2024-08-30 11:15:20 +0300

    Update the dummy experiment
    Fixed LSP diagnostic warnings.

--- src/experiments/dummy.py
+++ src/experiments/dummy.py
@@ -6,13 +6,16 @@ import tempfile
 import dotenv
 import lightning as L
 import torch
-import torch.nn as nn
 import torchinfo
 import torchvision
+from torch import nn
 
 
 class LitAutoEncoder(L.LightningModule):
+    """An example autoencoder."""
+
     def __init__(self, bottleneck_size: int = 64):
+        """Create a new LitAutoEncoder instance."""
         super().__init__()
         self.bottleneck_size = bottleneck_size
 
@@ -29,7 +32,8 @@ class LitAutoEncoder(L.LightningModule):
 
         self.save_hyperparameters()
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch, batch_idx):  # noqa: ARG002
+        """Process a single batch of the training dataset and return the loss."""
         x, y = batch
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
@@ -42,7 +46,8 @@ class LitAutoEncoder(L.LightningModule):
 
         return loss
 
-    def validation_step(self, batch, batch_idx):
+    def validation_step(self, batch, batch_idx):  # noqa: ARG002
+        """Process a single batch of the validation dataset and return the loss."""
         x, y = batch
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
@@ -51,11 +56,15 @@ class LitAutoEncoder(L.LightningModule):
         self.logger.log_metrics({"loss/val": loss.item()}, step=self.global_step)
 
     def configure_optimizers(self):
+        """Set up and return the optimizers."""
         return torch.optim.Adam(self.parameters(), lr=1e-3)
 
 
 class MNISTDataModule(L.LightningDataModule):
+    """An example data module for MNIST."""
+
     def __init__(self, data_dir: str, batch_size: int):
+        """Create a new MNISTDataModule instance."""
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
@@ -63,13 +72,11 @@ class MNISTDataModule(L.LightningDataModule):
 
     def prepare_data(self):
         """Prepare the data for setup (download, tokenize, etc.) on one device."""
-
         torchvision.datasets.MNIST(self.data_dir, train=True, download=True)
         torchvision.datasets.MNIST(self.data_dir, train=False, download=True)
 
     def setup(self, stage: str):
         """Prepare the data for training (split, transform, etc.) on all devices."""
-
         if stage == "fit":
             full_dataset = torchvision.datasets.MNIST(
                 self.data_dir,
@@ -91,6 +98,7 @@ class MNISTDataModule(L.LightningDataModule):
             raise NotImplementedError()
 
     def train_dataloader(self):
+        """Set up and return the train data loader."""
         return torch.utils.data.DataLoader(
             dataset=self.train,
             batch_size=self.batch_size,
@@ -98,6 +106,7 @@ class MNISTDataModule(L.LightningDataModule):
         )
 
     def val_dataloader(self):
+        """Set up and return the validation data loader."""
         return torch.utils.data.DataLoader(
             dataset=self.val,
             batch_size=self.batch_size,
@@ -105,9 +114,11 @@ class MNISTDataModule(L.LightningDataModule):
         )
 
     def test_dataloader(self):
+        """Set up and return the test data loader."""
         raise NotImplementedError()
 
     def predict_dataloader(self):
+        """Set up and return the prediction data loader."""
         raise NotImplementedError()
 
 
@@ -132,8 +143,8 @@ if __name__ == "__main__":
 
     summary = torchinfo.summary(autoencoder)
     with tempfile.TemporaryDirectory() as tmp:
-        tmp = f"{tmp}/model_summary.txt"
-        with open(tmp, "w") as f:
+        summary_file = f"{tmp}/model_summary.txt"
+        with open(summary_file, "w") as f:
             f.write(str(summary))
         logger.experiment.log_artifact(run_id=logger.run_id, local_path=tmp)
 
