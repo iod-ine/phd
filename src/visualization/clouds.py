a4960e057691bf4a3d4e9d61d5bf83f91ed4c3fb
Ivan Dubrovin <books.booksovich@yandex.com>
2024-09-07 17:04:23 +0300

    Add ability to 3D scatter cloud into existing axis

--- src/visualization/clouds.py
+++ src/visualization/clouds.py
@@ -5,6 +5,7 @@ import numpy as np
 def scatter_point_cloud_3d(
     xyz: np.ndarray,
     *,
+    ax=None,
     color=None,
     figsize=None,
     elev=None,
@@ -13,8 +14,9 @@ def scatter_point_cloud_3d(
 ) -> plt.Axes:
     """Create a 3D scatter plot of a point cloud."""
     color = color if color is not None else xyz[:, 2]
-    fig = plt.figure(figsize=figsize)
-    ax = fig.add_subplot(projection="3d")
+    if ax is None:
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(projection="3d")
     ax.view_init(elev=elev, azim=azim)
     ax.scatter(*np.rollaxis(xyz, 1), c=color, s=1, cmap=cmap)
     ax.set_aspect("equal")
