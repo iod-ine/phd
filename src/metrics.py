ac5650ee1c6a3c75e281cee2cad1de979e4c3f9e
Ivan Dubrovin <books.booksovich@yandex.com>
2024-09-08 17:50:40 +0300

    Add implementation of Intersection over Union

--- src/metrics.py
+++ src/metrics.py
@@ -21,3 +21,23 @@ class Accuracy(torchmetrics.Metric):
     def compute(self):
         """Use the internal state to compute the final value."""
         return self.correct / self.total
+
+
+class IntersectionOverUnion(torchmetrics.Metric):
+    """Micro-averaged intersection over union for tree segmentation."""
+
+    def __init__(self):
+        """Create a new metric instance."""
+        super().__init__()
+        self.add_state(name="intersection", default=torch.tensor(0))
+        self.add_state(name="union", default=torch.tensor(0))
+
+    def update(self, pred: torch.Tensor, target: torch.Tensor) -> None:
+        """Update the internal state."""
+        self.intersection += torch.sum(pred == target)
+        for i in target.unique():
+            self.union += torch.sum((pred == i) | (target == i))
+
+    def compute(self):
+        """Use the internal state to compute the final value."""
+        return self.intersection / self.union
