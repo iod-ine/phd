2e6be2b4caa6c4c364463730f64b859b3de151f6
Ivan Dubrovin <books.booksovich@yandex.com>
2024-09-17 20:24:49 +0300

    Update SyntheticForestRGBPatch to use base class

--- src/datasets/synthetic_forest_colored.py
+++ src/datasets/synthetic_forest_colored.py
@@ -178,7 +178,7 @@ class SyntheticForestRGBGrid(SyntheticForestRGBBase):
                     continue
 
                 row, col = transformer.rowcol(las.x, las.y)
-                rgb = ortho[:, row, col]  # shape: (3, n_points)
+                rgb = ortho[:, row, col].astype(np.float32)  # shape: (3, n_points)
 
                 xyzs.append(las.xyz)
                 features.append(np.swapaxes(rgb, 0, 1))
@@ -214,7 +214,7 @@ class SyntheticForestRGBGrid(SyntheticForestRGBBase):
         self.save(test_data, self.processed_paths[2])
 
 
-class SyntheticForestRGBPatch(torch_geometric.data.InMemoryDataset):
+class SyntheticForestRGBPatch(SyntheticForestRGBBase):
     """Colored synthetic forest generated from individual trees by patch."""
 
     def __init__(
@@ -263,24 +263,6 @@ class SyntheticForestRGBPatch(torch_geometric.data.InMemoryDataset):
             case "test":
                 self.load(self.processed_paths[2])
 
-    @functools.cached_property
-    def raw_file_names(self):
-        """List of files that need to be found in raw_dir to skip the download."""
-        return list(
-            itertools.chain.from_iterable(
-                [
-                    [f"Alder/alder_{i:>02}.las" for i in range(26)],
-                    [f"Aspen/aspen_{i:>02}.las" for i in range(73)],
-                    [f"Birch/birch_{i:>02}.las" for i in range(77)],
-                    [f"Fir/fir_{i:>02}.las" for i in range(37)],
-                    [f"Pine/pine_{i:>02}.las" for i in range(70)],
-                    [f"Spruce/spruce_{i:>02}.las" for i in range(94)],
-                    [f"Tilia/tilia_{i:>02}.las" for i in range(17)],
-                    ["ortho.tif"],
-                ],
-            )
-        )
-
     @property
     def processed_file_names(self):
         """List of files that need to be found in processed_dir to skip processing."""
@@ -297,57 +279,41 @@ class SyntheticForestRGBPatch(torch_geometric.data.InMemoryDataset):
         )
         return [f"sfc_{split}_{param_set_id}" for split in ("train", "val", "test")]
 
-    def download(self):
-        """Download raw data into raw_dir.
-
-        Notes:
-            Requires Kaggle API credentials in KAGGLE_USERNAME and KAGGLE_KEY
-            environment variables or ~/.kaggle/kaggle.json. For details, see
-            https://www.kaggle.com/docs/api#authentication.
-
-        """
-        kaggle.api.dataset_download_files(
-            dataset="sentinel3734/uav-point-clouds-of-individual-trees",
-            path=self.raw_dir,
-            unzip=True,
-        )
-        kaggle.api.dataset_download_file(
-            dataset="sentinel3734/tree-detection-lidar-rgb-full-images",
-            file_name="ortho.tif",
-            path=self.raw_dir,
-        )
-        archive = pathlib.Path(self.raw_dir) / "ortho.tif.zip"
-        with zipfile.ZipFile(archive) as z:
-            z.extractall(path=self.raw_dir)
-        archive.unlink()
-
     def process(self):
         """Process raw data and save it to processed_dir."""
-        raw_las_list = [laspy.read(p) for p in self.raw_paths if p.endswith(".las")]
-        processed_las_list = []
+        las_objects = [laspy.read(p) for p in self.raw_paths if p.endswith(".las")]
+        ortho_files = [p for p in self.raw_paths if p.endswith(".tif")]
 
-        with rasterio.open(pathlib.Path(self.raw_dir) / "ortho.tif") as dataset:
-            for raw_las in raw_las_list:
-                if not dataset.bounds.left < raw_las.xyz[0, 0] < dataset.bounds.right:
+        xyzs, features = [], []
+
+        for file in ortho_files:
+            with rasterio.open(pathlib.Path(file)) as dataset:
+                transformer = rasterio.transform.AffineTransformer(dataset.transform)
+                ortho = dataset.read()
+
+            for las in las_objects:
+                if not dataset.bounds.left < las.xyz[0, 0] < dataset.bounds.right:
                     continue
-                rgb = np.stack([c for c in dataset.sample(raw_las.xyz[:, :2])])
-                las = laspy.convert(raw_las, point_format_id=7)
-                las.red[:] = rgb[:, 0]
-                las.green[:] = rgb[:, 1]
-                las.blue[:] = rgb[:, 2]
-                processed_las_list.append(las)
+                if not dataset.bounds.bottom < las.xyz[0, 1] < dataset.bounds.top:
+                    continue
+
+                row, col = transformer.rowcol(las.x, las.y)
+                rgb = ortho[:, row, col].astype(np.float32)  # shape: (3, n_points)
+
+                xyzs.append(las.xyz)
+                features.append(np.swapaxes(rgb, 0, 1))
 
         data_list = []
         total_samples = self.train_samples + self.val_samples + self.test_samples
         random.seed(self.random_seed)
         for i in range(total_samples):
             pos, x, y = src.clouds.create_forest_patch(
-                las_list=processed_las_list,
+                xyzs=xyzs,
+                features=features,
                 width=self.patch_width,
                 height=self.patch_height,
                 overlap=self.patch_overlap,
                 height_threshold=self.height_threshold,
-                features_to_extract=self.las_features,
             )
             data = torch_geometric.data.Data(
                 pos=torch.from_numpy(pos.astype(np.float32)),
@@ -366,4 +332,4 @@ class SyntheticForestRGBPatch(torch_geometric.data.InMemoryDataset):
 
 
 if __name__ == "__main__":
-    dataset = SyntheticForestRGBGrid(root="data/tmp")
+    dataset = SyntheticForestRGBPatch(root="data/tmp")
