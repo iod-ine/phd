fe06ecd3b04bfbabe984791ef53773de3a00a0a3
Ivan Dubrovin <books.booksovich@yandex.com>
2024-09-17 20:04:56 +0300

    Add base class for RGB synthetic forest datasets
    The reuse the same code for downloading, it makes it harder to keep the
    implementations in sync. Now they simply inherit it.

--- src/datasets/synthetic_forest_colored.py
+++ src/datasets/synthetic_forest_colored.py
@@ -18,7 +18,74 @@ import src.clouds
 import src.datasets.utils
 
 
-class SyntheticForestRGBGrid(torch_geometric.data.InMemoryDataset):
+class SyntheticForestRGBBase(torch_geometric.data.InMemoryDataset):
+    """Base class for colored synthetic forest datasets."""
+
+    def __init__(
+        self,
+        root: str,
+        transform=None,
+        pre_transform=None,
+        pre_filter=None,
+        log: bool = True,
+        force_reload: bool = False,
+    ):
+        """Create a new instance."""
+        super().__init__(
+            root=root,
+            transform=transform,
+            pre_transform=pre_transform,
+            pre_filter=pre_filter,
+            log=log,
+            force_reload=force_reload,
+        )
+
+    @functools.cached_property
+    def raw_file_names(self):
+        """List of files that need to be found in raw_dir to skip the download."""
+        return list(
+            itertools.chain.from_iterable(
+                [
+                    [f"Alder/alder_{i:>02}.las" for i in range(26)],
+                    [f"Aspen/aspen_{i:>02}.las" for i in range(73)],
+                    [f"Birch/birch_{i:>02}.las" for i in range(77)],
+                    [f"Fir/fir_{i:>02}.las" for i in range(37)],
+                    [f"Pine/pine_{i:>02}.las" for i in range(70)],
+                    [f"Spruce/spruce_{i:>02}.las" for i in range(94)],
+                    [f"Tilia/tilia_{i:>02}.las" for i in range(17)],
+                    [f"plot_{i:>02}.tif" for i in range(1, 11)],
+                ],
+            )
+        )
+
+    def download(self):
+        """Download raw data into raw_dir.
+
+        Notes:
+            Requires Kaggle API credentials in KAGGLE_USERNAME and KAGGLE_KEY
+            environment variables or ~/.kaggle/kaggle.json. For details, see
+            https://www.kaggle.com/docs/api#authentication.
+
+        """
+        kaggle.api.dataset_download_files(
+            dataset="sentinel3734/uav-point-clouds-of-individual-trees",
+            path=self.raw_dir,
+            unzip=True,
+        )
+        for i in range(1, 11):
+            file = f"plot_{i:>02}.tif"
+            kaggle.api.dataset_download_file(
+                dataset="sentinel3734/tree-detection-lidar-rgb",
+                file_name=f"ortho/{file}",
+                path=self.raw_dir,
+            )
+            archive = pathlib.Path(self.raw_dir) / f"{file}.zip"
+            with zipfile.ZipFile(archive) as z:
+                z.extractall(path=self.raw_dir)
+            archive.unlink()
+
+
+class SyntheticForestRGBGrid(SyntheticForestRGBBase):
     """Colored synthetic forest generated from individual trees by grid."""
 
     def __init__(
@@ -73,24 +140,6 @@ class SyntheticForestRGBGrid(torch_geometric.data.InMemoryDataset):
             case "test":
                 self.load(self.processed_paths[2])
 
-    @functools.cached_property
-    def raw_file_names(self):
-        """List of files that need to be found in raw_dir to skip the download."""
-        return list(
-            itertools.chain.from_iterable(
-                [
-                    [f"Alder/alder_{i:>02}.las" for i in range(26)],
-                    [f"Aspen/aspen_{i:>02}.las" for i in range(73)],
-                    [f"Birch/birch_{i:>02}.las" for i in range(77)],
-                    [f"Fir/fir_{i:>02}.las" for i in range(37)],
-                    [f"Pine/pine_{i:>02}.las" for i in range(70)],
-                    [f"Spruce/spruce_{i:>02}.las" for i in range(94)],
-                    [f"Tilia/tilia_{i:>02}.las" for i in range(17)],
-                    [f"plot_{i:>02}.tif" for i in range(1, 11)],
-                ],
-            )
-        )
-
     @property
     def processed_file_names(self):
         """List of files that need to be found in processed_dir to skip processing."""
@@ -109,32 +158,6 @@ class SyntheticForestRGBGrid(torch_geometric.data.InMemoryDataset):
             self.las_features,
         )
         return [f"sfc_{split}_{param_set_id}" for split in ("train", "val", "test")]
-
-    def download(self):
-        """Download raw data into raw_dir.
-
-        Notes:
-            Requires Kaggle API credentials in KAGGLE_USERNAME and KAGGLE_KEY
-            environment variables or ~/.kaggle/kaggle.json. For details, see
-            https://www.kaggle.com/docs/api#authentication.
-
-        """
-        kaggle.api.dataset_download_files(
-            dataset="sentinel3734/uav-point-clouds-of-individual-trees",
-            path=self.raw_dir,
-            unzip=True,
-        )
-        for i in range(1, 11):
-            file = f"plot_{i:>02}.tif"
-            kaggle.api.dataset_download_file(
-                dataset="sentinel3734/tree-detection-lidar-rgb",
-                file_name=f"ortho/{file}",
-                path=self.raw_dir,
-            )
-            archive = pathlib.Path(self.raw_dir) / f"{file}.zip"
-            with zipfile.ZipFile(archive) as z:
-                z.extractall(path=self.raw_dir)
-            archive.unlink()
 
     def process(self):
         """Process raw data and save it to processed_dir."""
